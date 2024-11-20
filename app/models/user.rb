50ac4c096532f02b57e16aa1d5136be6cf841c78
Stephen Margheim <stephen.margheim@gmail.com>
2024-11-20 13:26:35 -0600

    Add model tests and get them passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+  validates :email_address, presence: true, uniqueness: true
 end
