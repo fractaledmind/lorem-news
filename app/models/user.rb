c01ea3641cf7ecbbe53f92f6d2672b96b6576602
Stephen Margheim <stephen.margheim@gmail.com>
2024-11-20 18:00:52 -0600

    Add application header with logo and authentication links

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ApplicationRecord
   normalizes :email_address, with: ->(e) { e.strip.downcase }
   validates :email_address, presence: true, uniqueness: true
   validates :password, length: { minimum: 8 }
+
+  def screen_name
+    email_address.split("@").first
+  end
+
+  def screen_name_in_database
+    email_address_in_database.split("@").first
+  end
 end
